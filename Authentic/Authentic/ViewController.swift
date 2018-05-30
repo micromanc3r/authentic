822563c8fb44929c8025339310a23423ef4a6f5b
Miroslav Sliacky <miroslav.sliacky@ahead-itec.com>
2018-05-30 21:03:30 +0200

    Create new project

---
+++ Authentic/Authentic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Authentic
+//
+//  Created by Miroslav Sliacky on 30/05/2018.
+//  Copyright © 2018 AHEAD iTec, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
